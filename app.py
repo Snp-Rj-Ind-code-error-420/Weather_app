302d0026ea2186b63d78b1567cc24e0e8824b5a6
Raja Sardar <80396878+Snp-Rj-Ind-code-error-420@users.noreply.github.com>
2024-03-23 16:44:02 +0530

    added search and button

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import ttkbootstrap as ttkb
 
 class Appn(ttkb.Window):
 	def __init__(self,title,size):
-		super().__init__(themename='minty')
+		super().__init__(themename='pulse')
 		# print(title)
 		self.title(title)
 		# self.style = Style('journal')s
@@ -15,7 +15,7 @@ class Appn(ttkb.Window):
 
 		#diff frame
 		self.head=Header(self)
-		# self.de
+		self.search=Search_frm(self)
 
 		# btn_1=ttkb.Button(self,text='btn1',bootstyle='primary')
 		# btn_1.pack()
@@ -24,7 +24,7 @@ class Appn(ttkb.Window):
 		self.mainloop()
 class Header(ttkb.Frame):
 	def __init__(self,parent):
-		super().__init__(parent,style='info') #style='info'
+		super().__init__(parent,style='primary') #style='info'
 		self.place(relx = 0.0, rely = 0.0, 
 			relwidth = 1, relheight = 0.15)
 		self.create_title()
@@ -33,11 +33,33 @@ class Header(ttkb.Frame):
 		lab1=ttkb.Label(self,text='Weather app', font=('JetBrains Mono NL', 20),style='info')
 		lab1.pack(side='left',expand=True,fill='both',padx=10,pady=10)
 
+class Search_frm(ttkb.Frame):
+	def __init__(self,parent):
+		super().__init__(parent,style='danger')
+		self.place(relx = 0.0, rely = 0.15, 
+			relwidth = 1, relheight = 0.15)
+		self.widgit()
+
+	def prnt(self):
+		print(self.input_city.get())
+	def widgit(self):
+		self.input_city = tk.StringVar()
+		self.columnconfigure((0,1,2),weight=1,uniform='a')
+		self.rowconfigure(0,weight=1,uniform='a')
+		entry1=ttkb.Entry(self,style='primary',textvariable=self.input_city,width=10 )
+		# entry1.insert('enter the city')
+		btn1=ttkb.Button(self,text='search',style='secondary',command=self.prnt)
+		entry1.grid(row=0,column=0,sticky='nsew',columnspan=2,padx = 20, pady = 10)
+		btn1.grid(row=0,column=2,sticky='nsew',padx = 20, pady = 10)
 
 
 
 
 
-Appn("ok gg",(600,600))
+
+
+
+
+Appn("Weather App",(600,400))
 
 
