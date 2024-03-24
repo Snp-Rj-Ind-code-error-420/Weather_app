cd263df62e0b4b1a3ce5ffbb36a6bcd065f04fbc
Raja Sardar <80396878+Snp-Rj-Ind-code-error-420@users.noreply.github.com>
2024-03-24 22:24:11 +0530

    added  degree c now time to add images

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from PIL import Image, ImageTk
 import tkinter as tk 
 import ttkbootstrap as ttkb  
 import json,time
+
+
 data=json.loads('{"coord":{"lon":86.1833,"lat":22.8},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50d"}],"base":"stations","main":{"temp":31.9,"feels_like":31.64,"temp_min":31.9,"temp_max":31.9,"pressure":1013,"humidity":37},"visibility":3000,"wind":{"speed":1.54,"deg":270},"clouds":{"all":20},"dt":1711258148,"sys":{"type":1,"id":9121,"country":"IN","sunrise":1711239320,"sunset":1711283254},"timezone":19800,"id":1269300,"name":"Jamshedpur","cod":200}')
+
+
+
+
 def prn():
 	#Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
 	print(f'temperature {data['main']['temp']}')
@@ -28,7 +34,7 @@ def prn():
 	print(f'date = {data['dt']} {time.gmtime(data['dt'])}\n {x[2]}/{x[1]}/{x[0]} {x[3]}:{x[4]}:{x[5]} ')
 
 
-	
+
 class Appn(ttkb.Window):
 	def __init__(self,title,size,*args, **kwargs):
 		super().__init__(themename='pulse')
@@ -66,32 +72,30 @@ class Base():
 		super().__init__(parent)
 
 
-	def widgit1(self):
-		print(self.degree.get())
-		self.columnconfigure((0,1,2),weight=1,uniform='b')
-		self.rowconfigure(0,weight=1,uniform='b')
-		self.python_dark = Image.open('sun-regular-24.png')	
-		lab101=ttkb.Label(self,text='', font=('JetBrains Mono NL', 20),style='primary')
-		lab101.grid(row=0,column=0,sticky='nsew',columnspan=2,padx = 20, pady = 10)
+
 
 
 
 class All_frm(ttkb.Frame):
 	def __init__(self,parent,*args, **kwargs):
 		super().__init__(parent,style='danger')
+
 		self.degree = tk.StringVar()	
+
 		self.place(relx = 0.0, rely = 0.15, 
 			relwidth = 1, relheight = 1)
+
+
 		frm1=ttkb.Frame(self,style='info')
-		frm1.place(relx = 0.0, rely = 0.0, 
-			relwidth = 1, relheight = 0.15)
+		frm1.place(relx = 0.0, rely = 0.0, relwidth = 1, relheight = 0.15)
+
+		frm2=ttkb.Frame(self,style='primary')
+		frm2.place(relx=0.0,rely=0.15,relwidth=1,relheight=0.24)
 
 		self.widgit2(frm1)
+		self.widgit1(frm2)
 
 
-	def prnt(self):
-		print(self.input_city.get())
-		# lab101['text']=data['main']['temp']
 
 
 	def widgit2(self,parent):
@@ -106,25 +110,24 @@ class All_frm(ttkb.Frame):
 
 		entry1.grid(row=0,column=0,sticky='nsew',columnspan=2,padx = 20, pady = 10)
 		btn1.grid(row=0,column=2,sticky='nsew',padx = 20, pady = 10)
-
-
-
-
-
-		# wit=base(self)
-
 	
 
-	# def prnt(self):
-	# 	print(self.input_city.get())
-		# lab101['text']=data['main']['temp']
+	def widgit1(self,parent):
+		# print(self.degree.get())
+		parent.columnconfigure((0,1,2),weight=1,uniform='b')
+		parent.rowconfigure(0,weight=1,uniform='b')
+		parent.python_dark = Image.open('sun-regular-24.png')	
+		self.lab101=ttkb.Label(parent,text='', font=('JetBrains Mono NL', 20),style='primary')
+		self.lab101.grid(row=0,column=0,sticky='nsew',columnspan=2,padx = 20, pady = 10)
 
-# class Display_frm(ttkb.Frame):
-# 	def __init__(self,parent,*args, **kwargs):
-# 		super().__init__(parent,style='info')
-# 		self.place(relx=0.0,rely=0.3,relwidth=1,relheight=0.30)
-		
-# 		self.widgit()
+
+	def prnt(self):
+		print(self.input_city.get())
+		self.lab101['text']=f'{data['main']['temp']}°C'
+
+
+
+
 
 
 
