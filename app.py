30c0a9a8f91e984433634ab2902fe1cbe4f64faf
Raja Sardar <80396878+Snp-Rj-Ind-code-error-420@users.noreply.github.com>
2024-03-24 22:02:48 +0530

    ola man

--- app.py
+++ app.py
@@ -27,8 +27,9 @@ def prn():
 	x=time.gmtime(data['dt'])
 	print(f'date = {data['dt']} {time.gmtime(data['dt'])}\n {x[2]}/{x[1]}/{x[0]} {x[3]}:{x[4]}:{x[5]} ')
 class Appn(ttkb.Window):
-	def __init__(self,title,size):
+	def __init__(self,title,size,*args, **kwargs):
 		super().__init__(themename='pulse')
+		# global degree
 		# print(title)
 		self.title(title)
 		# self.style = Style('journal')s
@@ -38,30 +39,15 @@ class Appn(ttkb.Window):
 
 		#diff frame
 		self.head=Header(self)
-		self.search=Search_frm(self)
-		self.dis=Display_frm(self)
+		self.search=All_frm(self)
+		# self.dis=Display_frm(self)
 		# btn_1=ttkb.Button(self,text='btn1',bootstyle='primary')
 		# btn_1.pack()
 
 
 		self.mainloop()
-
-class Display_frm(ttkb.Frame):
-	def __init__(self,parent):
-		super().__init__(parent,style='info')
-		self.place(relx=0.0,rely=0.3,relwidth=1,relheight=0.30)
-		self.widgit()
-
-
-	def widgit(self):
-		self.degree = tk.StringVar(value=None)
-		self.columnconfigure((0,1,2),weight=1,uniform='b')
-		self.rowconfigure(0,weight=1,uniform='b')
-		python_dark = Image.open('bx-sun.svg')	
-		lab1=ttkb.Label(self,text=self.degree, font=('JetBrains Mono NL', 20),style='primary')
-
 class Header(ttkb.Frame):
-	def __init__(self,parent):
+	def __init__(self,parent,*args, **kwargs):
 		super().__init__(parent,style='') #style='info'
 		self.place(relx = 0.0, rely = 0.0, 
 			relwidth = 1, relheight = 0.15)
@@ -71,19 +57,22 @@ class Header(ttkb.Frame):
 		lab1=ttkb.Label(self,text='Weather app', font=('JetBrains Mono NL', 20),style='primary')
 		lab1.pack(side='left',expand=True,fill='both',padx=10,pady=10)
 
-class Search_frm(ttkb.Frame):
-	def __init__(self,parent):
-		super().__init__(parent,style='danger')
-		self.place(relx = 0.0, rely = 0.15, 
-			relwidth = 1, relheight = 0.15)
-		self.widgit()
 
-	def prnt(self):
-		print(self.input_city.get())
-		self.degree.set(data['main']['temp'])
+class Base():
+	def __init__(self,parent,*args, **kwargs):
+		super().__init__(parent)
 
 
-	def widgit(self):
+	def widgit1(self):
+		print(self.degree.get())
+		self.columnconfigure((0,1,2),weight=1,uniform='b')
+		self.rowconfigure(0,weight=1,uniform='b')
+		self.python_dark = Image.open('sun-regular-24.png')	
+		lab101=ttkb.Label(self,text='', font=('JetBrains Mono NL', 20),style='primary')
+		lab101.grid(row=0,column=0,sticky='nsew',columnspan=2,padx = 20, pady = 10)
+
+
+	def widgit2(self):
 		self.input_city = tk.StringVar(value=None)
 		self.columnconfigure((0,1,2),weight=1,uniform='a')
 		self.rowconfigure(0,weight=1,uniform='a')
@@ -95,6 +84,58 @@ class Search_frm(ttkb.Frame):
 
 		entry1.grid(row=0,column=0,sticky='nsew',columnspan=2,padx = 20, pady = 10)
 		btn1.grid(row=0,column=2,sticky='nsew',padx = 20, pady = 10)
+
+
+class All_frm(ttkb.Frame):
+	def __init__(self,parent,*args, **kwargs):
+		super().__init__(parent,style='danger')
+		self.degree = tk.StringVar()	
+		self.place(relx = 0.0, rely = 0.15, 
+			relwidth = 1, relheight = 1)
+		frm1=ttkb.Frame(self,style='info')
+		frm1.place(relx = 0.0, rely = 0.0, 
+			relwidth = 1, relheight = 0.15)
+
+		self.widgit2(frm1)
+
+
+	def prnt(self):
+		print(self.input_city.get())
+		# lab101['text']=data['main']['temp']
+
+
+	def widgit2(self,parent):
+		self.input_city = tk.StringVar(value=None)
+		parent.columnconfigure((0,1,2),weight=1,uniform='a')
+		parent.rowconfigure(0,weight=1,uniform='a')
+
+		entry1=ttkb.Entry(parent,style='primary',textvariable=self.input_city,width=10)
+		btn1=ttkb.Button(parent,text='search',style='info.Outline',command=self.prnt)
+
+		# entry1.insert('enter the city')
+
+		entry1.grid(row=0,column=0,sticky='nsew',columnspan=2,padx = 20, pady = 10)
+		btn1.grid(row=0,column=2,sticky='nsew',padx = 20, pady = 10)
+
+
+
+
+
+		# wit=base(self)
+
+	
+
+	# def prnt(self):
+	# 	print(self.input_city.get())
+		# lab101['text']=data['main']['temp']
+
+# class Display_frm(ttkb.Frame):
+# 	def __init__(self,parent,*args, **kwargs):
+# 		super().__init__(parent,style='info')
+# 		self.place(relx=0.0,rely=0.3,relwidth=1,relheight=0.30)
+		
+# 		self.widgit()
+
 
 
 
